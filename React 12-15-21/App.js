b364676fb10056d1d9aeda2164ba4e66ba69acf4
Chitipat111 <87640233+Chitipat111@users.noreply.github.com>
2021-12-15 11:43:11 +0700

    12/15/21 Commit
    12/15/21 Commit

---
+++ React 12-15-21/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text, Modal } from 'react-native'
+import AlertExample from './Components/W4/AlertExample'
+import ModalExample from './Components/W4/ModalExample'
+
+const App = () => {
+  return (
+    // <AlertExample/>
+    <ModalExample/>
+  )
+}
+
+export default App
