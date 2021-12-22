2445a7df82f37b0aaca43bde831005cb50edd031
Chitipat111 <87640233+Chitipat111@users.noreply.github.com>
2021-12-22 11:53:15 +0700

    WebView and Stack Navigation

--- App.js
+++ App.js
@@ -1,12 +1,43 @@
+// import React from 'react'
+// import { View, Text } from 'react-native'
+// import WebViewExample from './components/WebViewExample'
+// import WebViewLoading from './components/WebViewLoading'
+
+// const App = () => {
+//   return (
+//     // <WebViewExample/>
+//     <WebViewLoading/>
+//   )
+// }
+
+// export default App
+
 import React from 'react'
-import { View, Text, Modal } from 'react-native'
-import AlertExample from './Components/W4/AlertExample'
-import ModalExample from './Components/W4/ModalExample'
+import { View, Text } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen'
+import AboutScreen from './screens/AboutScreen'
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    // <AlertExample/>
-    <ModalExample/>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#32C6E6',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}>
+        <Stack.Screen name="Home" component={HomeScreen} option={{title:'หน้าหลัก'}}/>
+        <Stack.Screen name="About" component={AboutScreen} option={{title:'เกี่ยวกับเรา'}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
