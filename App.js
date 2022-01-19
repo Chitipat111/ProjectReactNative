917473970346ad0894ba63f2b810bc763866e441
Chitipat111 <87640233+Chitipat111@users.noreply.github.com>
2022-01-19 20:46:31 +0700

    19/01/22 Header ( button & icon )

--- App.js
+++ App.js
@@ -4,15 +4,13 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen'
 import AboutScreen from './screens/AboutScreen'
-import FirstScreen from './screens/FirstScreen'
-import SecondScreen from './screens/SecondScreen'
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='FirstPage'
+      <Stack.Navigator initialRouteName='Home'
       screenOptions={{
         headerStyle: {
           backgroundColor: '#32C6E6',
@@ -22,8 +20,8 @@ const App = () => {
           fontWeight: 'bold',
         },
       }}>
-        <Stack.Screen name="FirstPage" component={FirstScreen} option={{title:'First Page'}}/>
-        <Stack.Screen name="SecondPage" component={SecondScreen} option={{title:'Second Page'}}/>
+        <Stack.Screen name="Home" component={HomeScreen} option={{title:'หน้าหลัก'}}/>
+        <Stack.Screen name="About" component={AboutScreen} option={{title:'เกี่ยวกับเรา'}}/>
       </Stack.Navigator>
     </NavigationContainer>
   )
